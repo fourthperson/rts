50c36892bfdbaebf3ddc1f9340d3ae1a2aca04b6
fourthperson <iankariithi@gmail.com>
2023-05-24 16:58:09 +0300

    attempting to call  native method from react

---
+++ android/app/src/main/java/com/testproject/packages/GoCheck.java
@@ -0,0 +1,40 @@
+package com.testproject;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class GoCheck extends ReactContextBaseJavaModule {
+
+    private final Context context;
+    private ActivityManager manager;
+
+    public GoCheck(ReactApplicationContext context) {
+        super(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "GoDeviceCheck";
+    }
+
+    @ReactMethod
+    public boolean isAndroidGoDevice() {
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.Q && manager(context).isLowRamDevice();
+    }
+
+    private ActivityManager manager(Context context) {
+        if (manager == null) {
+            manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        }
+        return manager;
+    }
+}
---
+++ android/app/src/main/java/com/testproject/packages/GoCheckPackager.java
@@ -0,0 +1,27 @@
+package com.testproject;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoCheckPackager implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactApplicationContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new GoCheck(reactApplicationContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactApplicationContext) {
+        return new ArrayList<>();
+    }
+}
