8c154bbab1c2a79324a88495d39826e8055debec
fourthperson <iankariithi@gmail.com>
2023-06-03 21:45:14 +0300

    used eslint instead of prettier,

--- App.jsx
+++ App.jsx
@@ -6,10 +6,10 @@
  */
 
 import React from 'react';
-import {DeviceEventEmitter} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {reopenApp} from 'react-native-floating-bubble';
+import { DeviceEventEmitter } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { reopenApp } from 'react-native-floating-bubble';
 
 import SplashPage from './src/pages/splash';
 import HomePage from './src/pages/home';
@@ -18,26 +18,26 @@ import MapPage from './src/pages/map';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{headerShown: false}}
-        initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={SplashPage} />
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Map" component={MapPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{ headerShown: false }}
+				initialRouteName="Splash">
+				<Stack.Screen name="Splash" component={SplashPage} />
+				<Stack.Screen name="Home" component={HomePage} />
+				<Stack.Screen name="Map" component={MapPage} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 DeviceEventEmitter.addListener('floating-bubble-press', e => {
-  // What to do when user press the bubble
-  reopenApp();
+	// What to do when user press the bubble
+	reopenApp();
 });
 DeviceEventEmitter.addListener('floating-bubble-remove', e => {
-  // What to do when user removes the bubble
-  console.log('Remove Bubble');
+	// What to do when user removes the bubble
+	console.log('Remove Bubble');
 });
 
 export default App;
--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,135 +1,110 @@
-import React, {useEffect, useState} from 'react';
-import {PermissionsAndroid, StatusBar, StyleSheet, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { PermissionsAndroid, StatusBar, StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import VIForegroundService from '@voximplant/react-native-foreground-service';
-// import BackgroundGeolocation from 'react-native-background-geolocation';
 import Geolocation from 'react-native-geolocation-service';
 
-const MapPage = ({route, navigation}) => {
-  const [permissionGranted, setPermissionGranted] = useState(null);
-  const [location, setLocation] = useState(null);
+const MapPage = ({ route, navigation }) => {
+	const [permissionGranted, setPermissionGranted] = useState(null);
+	const [location, setLocation] = useState(null);
 
-  const requestLocationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Cool App Camera Permission',
-          message: 'Cool App needs access to your location',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      setPermissionGranted(granted === PermissionsAndroid.RESULTS.GRANTED);
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the Location');
-        // start location tracking
+	const requestLocationPermission = async () => {
+		try {
+			const granted = await PermissionsAndroid.request(
+				PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+				{
+					title: 'Cool App Camera Permission',
+					message: 'Cool App needs access to your location',
+					buttonNeutral: 'Ask Me Later',
+					buttonNegative: 'Cancel',
+					buttonPositive: 'OK',
+				},
+			);
+			setPermissionGranted(granted === PermissionsAndroid.RESULTS.GRANTED);
+			if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+				console.log('You can use the Location');
+				// start location tracking
 
-        // BackgroundGeolocation.ready(
-        //   {
-        //     desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
-        //     distanceFilter: 10,
-        //     stopTimeout: 1,
-        //     stopOnTerminate: false,
-        //     startOnBoot: false,
-        //   },
-        //   state => {
-        //     if (!state.enabled) {
-        //       BackgroundGeolocation.start();
-        //     }
-        //   },
-        // );
+				Geolocation.watchPosition(
+					position => {
+						console.log(position);
+						setLocation(location);
+					},
+					error => {
+						console.log(error.code, error.message);
+					},
+					{ enableHighAccuracy: true, distanceFilter: 50 },
+				);
+			} else {
+				console.log('Location permission denied');
+			}
+		} catch (err) {
+			console.warn(err);
+		}
+	};
 
-        Geolocation.watchPosition(
-          position => {
-            console.log(position);
-            setLocation(location);
-          },
-          error => {
-            console.log(error.code, error.message);
-          },
-          {enableHighAccuracy: true, distanceFilter: 50, },
-        );
-      } else {
-        console.log('Location permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
+	const channelConfig = {
+		id: 'channelId',
+		name: 'Channel name',
+		description: 'Channel description',
+		enableVibration: false,
+	};
 
-  const channelConfig = {
-    id: 'channelId',
-    name: 'Channel name',
-    description: 'Channel description',
-    enableVibration: false,
-  };
+	const notificationConfig = {
+		channelId: 'channelId',
+		id: 3456,
+		title: 'Title',
+		text: 'Some text',
+		icon: 'ic_icon',
+		button: 'Some text',
+	};
 
-  const notificationConfig = {
-    channelId: 'channelId',
-    id: 3456,
-    title: 'Title',
-    text: 'Some text',
-    icon: 'ic_icon',
-    button: 'Some text',
-  };
+	useEffect(() => {
+		requestLocationPermission();
+	}, []);
 
-  // BackgroundGeolocation.onLocation(
-  //   location => {
-  //     console.log('[location] ' + location);
-  //     setLocation(location);
-  //   },
-  //   error => {
-  //     console.log('[location.error] ' + error);
-  //   },
-  // );
+	useEffect(() => {
+		async function createChannel() {
+			if (permissionGranted) {
+				await VIForegroundService.getInstance().createNotificationChannel(
+					channelConfig,
+				);
+				console.log('Foreground service channel created');
+				await VIForegroundService.getInstance().startService(
+					notificationConfig,
+				);
+			}
+		}
 
-  useEffect(() => {
-    requestLocationPermission();
-  }, []);
+		console.log('Has permission: ' + permissionGranted);
+		createChannel().then(r => console.log(r));
+	}, [permissionGranted]);
 
-  useEffect(() => {
-    async function createChannel() {
-      if (permissionGranted) {
-        await VIForegroundService.getInstance().createNotificationChannel(
-          channelConfig,
-        );
-        console.log('Foreground service channel created');
-        await VIForegroundService.getInstance().startService(
-          notificationConfig,
-        );
-      }
-    }
-    console.log('Has permission: ' + permissionGranted);
-    createChannel();
-  }, [permissionGranted]);
-
-  return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor="orange" />
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        region={{
-          latitude: -1.286389,
-          longitude: 36.817223,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StatusBar backgroundColor="orange" />
+			<MapView
+				provider={PROVIDER_GOOGLE}
+				style={styles.map}
+				region={{
+					latitude: -1.286389,
+					longitude: 36.817223,
+					latitudeDelta: 0.015,
+					longitudeDelta: 0.0121,
+				}}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#ffffff',
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
 });
 
 export default MapPage;
